ad8b820f0bbe19ca9807ff9930ead941cf5b52cc
Christopher DeLeon <lionscantcode@gmail.com>
2020-05-29 21:34:56 -0500

    First Commit

---
+++ Donut Solution/Donut Project/frmMain.Designer.vb
@@ -0,0 +1,304 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.radFilled = New System.Windows.Forms.RadioButton()
+        Me.radChocolate = New System.Windows.Forms.RadioButton()
+        Me.radSugar = New System.Windows.Forms.RadioButton()
+        Me.radGlazed = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.radCappuccino = New System.Windows.Forms.RadioButton()
+        Me.radRegular = New System.Windows.Forms.RadioButton()
+        Me.radNone = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.txtSubTotal = New System.Windows.Forms.TextBox()
+        Me.txtTotalDue = New System.Windows.Forms.TextBox()
+        Me.txtSalesTax = New System.Windows.Forms.TextBox()
+        Me.lblSalesTax = New System.Windows.Forms.Label()
+        Me.lblTotalDue = New System.Windows.Forms.Label()
+        Me.lblSubTotal = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radFilled)
+        Me.GroupBox1.Controls.Add(Me.radChocolate)
+        Me.GroupBox1.Controls.Add(Me.radSugar)
+        Me.GroupBox1.Controls.Add(Me.radGlazed)
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(65, 61)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(138, 159)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Doughnut Choices"
+        '
+        'radFilled
+        '
+        Me.radFilled.AutoSize = True
+        Me.radFilled.Location = New System.Drawing.Point(12, 118)
+        Me.radFilled.Name = "radFilled"
+        Me.radFilled.Size = New System.Drawing.Size(91, 19)
+        Me.radFilled.TabIndex = 3
+        Me.radFilled.TabStop = True
+        Me.radFilled.Text = "&Filled ($1.50)"
+        Me.radFilled.UseVisualStyleBackColor = True
+        '
+        'radChocolate
+        '
+        Me.radChocolate.AutoSize = True
+        Me.radChocolate.Location = New System.Drawing.Point(12, 85)
+        Me.radChocolate.Name = "radChocolate"
+        Me.radChocolate.Size = New System.Drawing.Size(117, 19)
+        Me.radChocolate.TabIndex = 2
+        Me.radChocolate.TabStop = True
+        Me.radChocolate.Text = "Ch&ocolate ($1.25)"
+        Me.radChocolate.UseVisualStyleBackColor = True
+        '
+        'radSugar
+        '
+        Me.radSugar.AutoSize = True
+        Me.radSugar.Location = New System.Drawing.Point(12, 52)
+        Me.radSugar.Name = "radSugar"
+        Me.radSugar.Size = New System.Drawing.Size(93, 19)
+        Me.radSugar.TabIndex = 1
+        Me.radSugar.TabStop = True
+        Me.radSugar.Text = "&Sugar ($1.05)"
+        Me.radSugar.UseVisualStyleBackColor = True
+        '
+        'radGlazed
+        '
+        Me.radGlazed.AutoSize = True
+        Me.radGlazed.Location = New System.Drawing.Point(12, 19)
+        Me.radGlazed.Name = "radGlazed"
+        Me.radGlazed.Size = New System.Drawing.Size(98, 19)
+        Me.radGlazed.TabIndex = 0
+        Me.radGlazed.TabStop = True
+        Me.radGlazed.Text = "&Glazed ($1.05)"
+        Me.radGlazed.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.radCappuccino)
+        Me.GroupBox2.Controls.Add(Me.radRegular)
+        Me.GroupBox2.Controls.Add(Me.radNone)
+        Me.GroupBox2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(65, 233)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(147, 116)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Coffee Choices"
+        '
+        'radCappuccino
+        '
+        Me.radCappuccino.AutoSize = True
+        Me.radCappuccino.Location = New System.Drawing.Point(12, 85)
+        Me.radCappuccino.Name = "radCappuccino"
+        Me.radCappuccino.Size = New System.Drawing.Size(127, 19)
+        Me.radCappuccino.TabIndex = 2
+        Me.radCappuccino.Text = "Ca&ppuccino ($2.75)"
+        Me.radCappuccino.UseVisualStyleBackColor = True
+        '
+        'radRegular
+        '
+        Me.radRegular.AutoSize = True
+        Me.radRegular.Location = New System.Drawing.Point(12, 52)
+        Me.radRegular.Name = "radRegular"
+        Me.radRegular.Size = New System.Drawing.Size(103, 19)
+        Me.radRegular.TabIndex = 1
+        Me.radRegular.Text = "&Regular ($1.50)"
+        Me.radRegular.UseVisualStyleBackColor = True
+        '
+        'radNone
+        '
+        Me.radNone.AutoSize = True
+        Me.radNone.Checked = True
+        Me.radNone.Location = New System.Drawing.Point(12, 19)
+        Me.radNone.Name = "radNone"
+        Me.radNone.Size = New System.Drawing.Size(57, 19)
+        Me.radNone.TabIndex = 0
+        Me.radNone.TabStop = True
+        Me.radNone.Text = "&None "
+        Me.radNone.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(462, 30)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Our doughnuts and coffee are the best in town!"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.txtSubTotal)
+        Me.GroupBox3.Controls.Add(Me.txtTotalDue)
+        Me.GroupBox3.Controls.Add(Me.txtSalesTax)
+        Me.GroupBox3.Controls.Add(Me.lblSalesTax)
+        Me.GroupBox3.Controls.Add(Me.lblTotalDue)
+        Me.GroupBox3.Controls.Add(Me.lblSubTotal)
+        Me.GroupBox3.Location = New System.Drawing.Point(228, 61)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(174, 142)
+        Me.GroupBox3.TabIndex = 6
+        Me.GroupBox3.TabStop = False
+        '
+        'txtSubTotal
+        '
+        Me.txtSubTotal.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.txtSubTotal.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSubTotal.Location = New System.Drawing.Point(79, 18)
+        Me.txtSubTotal.Name = "txtSubTotal"
+        Me.txtSubTotal.ReadOnly = True
+        Me.txtSubTotal.Size = New System.Drawing.Size(51, 23)
+        Me.txtSubTotal.TabIndex = 16
+        Me.txtSubTotal.TabStop = False
+        '
+        'txtTotalDue
+        '
+        Me.txtTotalDue.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.txtTotalDue.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTotalDue.Location = New System.Drawing.Point(79, 87)
+        Me.txtTotalDue.Name = "txtTotalDue"
+        Me.txtTotalDue.ReadOnly = True
+        Me.txtTotalDue.Size = New System.Drawing.Size(51, 23)
+        Me.txtTotalDue.TabIndex = 15
+        Me.txtTotalDue.TabStop = False
+        '
+        'txtSalesTax
+        '
+        Me.txtSalesTax.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.txtSalesTax.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSalesTax.Location = New System.Drawing.Point(79, 52)
+        Me.txtSalesTax.Name = "txtSalesTax"
+        Me.txtSalesTax.ReadOnly = True
+        Me.txtSalesTax.Size = New System.Drawing.Size(51, 23)
+        Me.txtSalesTax.TabIndex = 13
+        Me.txtSalesTax.TabStop = False
+        '
+        'lblSalesTax
+        '
+        Me.lblSalesTax.AutoSize = True
+        Me.lblSalesTax.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSalesTax.Location = New System.Drawing.Point(18, 54)
+        Me.lblSalesTax.Name = "lblSalesTax"
+        Me.lblSalesTax.Size = New System.Drawing.Size(59, 15)
+        Me.lblSalesTax.TabIndex = 2
+        Me.lblSalesTax.Text = "Sales Tax: "
+        '
+        'lblTotalDue
+        '
+        Me.lblTotalDue.AutoSize = True
+        Me.lblTotalDue.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalDue.Location = New System.Drawing.Point(18, 89)
+        Me.lblTotalDue.Name = "lblTotalDue"
+        Me.lblTotalDue.Size = New System.Drawing.Size(62, 15)
+        Me.lblTotalDue.TabIndex = 1
+        Me.lblTotalDue.Text = "Total Due: "
+        '
+        'lblSubTotal
+        '
+        Me.lblSubTotal.AutoSize = True
+        Me.lblSubTotal.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSubTotal.Location = New System.Drawing.Point(18, 19)
+        Me.lblSubTotal.Name = "lblSubTotal"
+        Me.lblSubTotal.Size = New System.Drawing.Size(61, 15)
+        Me.lblSubTotal.TabIndex = 0
+        Me.lblSubTotal.Text = "Sub Total: "
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalc.Location = New System.Drawing.Point(307, 233)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(95, 37)
+        Me.btnCalc.TabIndex = 2
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(307, 276)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(95, 37)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.ClientSize = New System.Drawing.Size(486, 377)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Donut Shoppe"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents radFilled As RadioButton
+    Friend WithEvents radChocolate As RadioButton
+    Friend WithEvents radSugar As RadioButton
+    Friend WithEvents radGlazed As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents radCappuccino As RadioButton
+    Friend WithEvents radRegular As RadioButton
+    Friend WithEvents radNone As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents txtTotalDue As TextBox
+    Friend WithEvents txtSalesTax As TextBox
+    Friend WithEvents lblSalesTax As Label
+    Friend WithEvents lblTotalDue As Label
+    Friend WithEvents lblSubTotal As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtSubTotal As TextBox
+End Class
---
+++ Donut Solution/Donut Project/frmMain.vb
@@ -0,0 +1,108 @@
+﻿' Name:         Doughnut Project
+' Purpose:      Doughnut calculator
+' Programmer:   Christopher DeLeon on 3.15.2020
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+    '  Procedures to find Subtotal
+    Private Function doughnut() As Double
+        ' function to calculate and return donut price
+        Dim x As Double
+
+        ' chooses which ever radio button is checked
+        If radGlazed.Checked = True Then
+            x = 1.05
+        ElseIf radSugar.Checked = True Then
+            x = 1.05
+        ElseIf radChocolate.Checked = True Then
+            x = 1.25
+        ElseIf radFilled.Checked = True Then
+            x = 1.5
+        End If
+        Return x
+    End Function
+
+
+    Private Function coffee() As Double
+        ' function to calculate and return coffee price
+        Dim y As Double
+
+        'chooses which ever radio button is checked
+        If radRegular.Checked = True Then
+            y = 1.5
+        ElseIf radCappuccino.Checked = True Then
+            y = 2.75
+        ElseIf radNone.Checked = True Then
+            y = 0
+        End If
+        Return y
+    End Function
+
+    'Function to find Sales Tax
+    Private Function calc_tax(ByVal z As Double) As Double
+
+        'function to calculate tax on subtotal and return tax
+        Return z * (6 / 100)
+    End Function
+
+    'Calculate
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+
+        Dim doughnut_price As Double = 0
+        Dim coffee_price As Double = 0
+        Dim sub_total, tax, total As Double
+
+        'calling functions and assigning values to variables
+        doughnut_price = doughnut()
+        coffee_price = coffee()
+
+        ' calculate subtotal
+        sub_total = doughnut_price + coffee_price
+
+        ' calculate tax
+        tax = calc_tax(sub_total)
+
+        ' calculate total due
+        total = sub_total + tax
+
+        ' display sub total, tax and total due
+        txtSubTotal.Text = "$" + sub_total.ToString("#.##")
+        txtSalesTax.Text = "$" + tax.ToString("#.##")
+        txtTotalDue.Text = "$" + total.ToString("#.##")
+
+    End Sub
+
+    'Exit Confirmation
+    Private Sub frmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        Dim dlgButton As DialogResult
+        dlgButton = MessageBox.Show("Do you want to exit?", "Donut Shoppe", MessageBoxButtons.YesNo,
+                                    MessageBoxIcon.Exclamation)
+
+        'If the No button selected, Do not close the form
+        If dlgButton = DialogResult.No Then
+            e.Cancel = True
+        End If
+    End Sub
+
+    ' Exit button
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    'Clear output once new menu items are checked
+    Private Sub radGlazed_CheckedChanged(sender As Object, e As EventArgs) Handles radGlazed.CheckedChanged,
+                                                                            radSugar.CheckedChanged, radChocolate.CheckedChanged, radFilled.CheckedChanged,
+                                                                            radNone.CheckedChanged, radRegular.CheckedChanged, radCappuccino.TextChanged
+        txtSalesTax.Text = String.Empty
+        txtSubTotal.Text = String.Empty
+        txtTotalDue.Text = String.Empty
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
